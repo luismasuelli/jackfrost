df4e5d126908347e7c0382305bc2ef93eddcae6d
Luis <luismasuelli@hotmail.com>
2013-08-14 20:18:40 -0300

    setup

--- setup.py
+++ setup.py
@@ -5,8 +5,15 @@ setup(
     version='0.5',
     packages=['jackfrost'],
     url='',
-    license='',
+    license='GPL',
     author='Luis Masuelli',
     author_email='luismasuelli@hotmail.com',
-    description='Autocomplete feature for django. Allows you to serve a channel as a datasource to select an option from a large amounth without need to hit the whole DB set.'
+    description='Autocomplete feature for django. Allows you to serve a channel as a datasource to select an option from a large amounth without need to hit the whole DB set.',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Framework :: Django',
+        'Intended Audience :: Developers'
+    ]
 )
