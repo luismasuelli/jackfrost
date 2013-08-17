a014d18a90c78760a7cde1035f6ecebd06b50e32
Luis <luismasuelli@hotmail.com>
2013-08-17 17:25:35 -0300

    0.5.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='jackfrost',
-    version='0.5.1',
+    version='0.5.2',
     packages=['jackfrost'],
     package_data={'jackfrost': [os.path.join('static', 'css', 'themes', 'base', 'images', '*.png'),
                                 os.path.join('static', 'css', 'themes', 'base', '*.css'),
