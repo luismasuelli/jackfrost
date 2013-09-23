80289605e57696f73a27f89e79e276fe92216429
Luis <luismasuelli@hotmail.com>
2013-09-22 23:32:02 -0300

    new local password.

--- jackfrost_app/settings.py
+++ jackfrost_app/settings.py
@@ -17,7 +17,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'jackfrost_test',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'aamd532.dll',                  # Not used with sqlite3.
+        'PASSWORD': 'root1',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
